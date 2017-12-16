3f901f8474286d65959841feeaf8a74e2c102246
Guillaume <gmouchet@gmail.com>
2017-12-16 21:53:09 +0100

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -3,13 +3,13 @@ var rayon_km = 20;
 
 
 // Bayonne
-var latitude = 43.4833;
-var longitude = -1.4833;
+//var latitude = 43.4833;
+//var longitude = -1.4833;
 
 // Paris
 // var latitude = 48.8534;
 // var longitude = 2.3488;
 
 // Lyon
-// var latitude = 45.75;
-// var longitude = 4.85;
+var latitude = 45.75;
+var longitude = 4.85;
