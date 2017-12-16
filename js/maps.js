aa3d1d65b07f7c0f6a6e7fcdd1a8c977a5f5fc1d
amart <gmouchet@gmail.com>
2017-12-16 19:30:52 +0100

    creation maps.js

---
+++ js/maps.js
@@ -0,0 +1,15 @@
+var zoom_carte = 11; // 11 par defaut
+var rayon_km = 20;
+
+
+// Bayonne
+// var latitude = 43.4833;
+// var longitude = -1.4833;
+
+// Paris
+// var latitude = 48.8534;
+// var longitude = 2.3488;
+
+// Lyon
+var latitude = 45.75;
+var longitude = 4.85;
