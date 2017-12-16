618adffd05bf64c90e44fe27dc7b9b49be68de18
Guillaume <gmouchet@gmail.com>
2017-12-16 20:07:09 +0100

    Update maps.js
    test update maps.js

--- js/maps.js
+++ js/maps.js
@@ -7,9 +7,9 @@ var rayon_km = 20;
 // var longitude = -1.4833;
 
 // Paris
-// var latitude = 48.8534;
-// var longitude = 2.3488;
+var latitude = 48.8534;
+var longitude = 2.3488;
 
 // Lyon
-var latitude = 45.75;
-var longitude = 4.85;
+// var latitude = 45.75;
+// var longitude = 4.85;
